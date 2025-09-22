6e0a17ab53577d2af4c364d8cb1bdb5d32609a1c
ishardcoded <ishardcoded@gmail.com>
2025-09-22 18:26:48 +0500

    Fix

--- frontend/src/hooks/useUser.ts
+++ frontend/src/hooks/useUser.ts
@@ -4,19 +4,13 @@ import type { User } from '../shared/types/User'
 
 export function useUser() {
 	const [users, setUsers] = useState<User[]>([])
-	const [error, setError] = useState<unknown | null>(null)
 
 	async function getAllUsers(token: string | null) {
-		try {
-			setUsers(await USER_SERVICE.getAllUsers(token))
-		} catch (e) {
-			setError(e)
-		}
+		setUsers(await USER_SERVICE.getAllUsers(token))
 	}
 
 	return {
 		users,
-		error,
 		getAllUsers,
 	}
 }
